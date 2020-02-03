426f5a45ee05dd0ce40df92106d58804b4c382a6
Dan <dan.demart@laposte.net>
2020-02-03 18:18:01 +0100

    save

---
+++ app.js
@@ -0,0 +1,18 @@
+$('#play').hide();
+
+let id = Date.now();
+
+$('#player-btn').click(function() {
+  let playerName = $('#player-id').val();
+    let playerList = '';
+
+
+      playerList +=
+        '<p id='+id+' style="font-size:14px;">'+playerName+'</p>';
+          $('.list').append(playerList);
+
+          for (var i = 0; i < id; i++) {
+            $.jStorage.set("id_player" + id, playerName)
+          }
+
+})
