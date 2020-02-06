fc83011ee53314395b10f59a352780bee1c127d0
FoobarIT <dan.demart@laposte.net>
2020-02-06 15:04:54 +0100

    Storage the players

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
 $('#play').hide();
 
 
+
+
+let uid = 0;
+function generateID() {
+  uid += 1
+}
+
 $('#player-btn').click(function() {
+  generateID()
 const score = 0
 const player = $('#player-id').val()
+let test = $.jStorage.index()
 
-let ref = Date.now();
-  let test = {
+  let data = {
     player,
     score
   }
-  $.jStorage.set('ref:'+ref+'', test);
+  $.jStorage.set('data-'+uid, data);
+  $.jStorage.reInit()
 
   let playerList = ''
-    playerList += '<p style="font-size:14px">'+test.player+'</p>'
+    playerList += '<p style="font-size:14px">'+data.player+'</p>'
 
   $('.list').append(playerList)
-        // for (var i = 0; i < playerName; i++) {
-        //   $.jStorage.set("player" + i, players_DB[i]);
-        // }
+
+      console.log(test.length)
+
+      if (test.length > 0 ) {
+        $('#play').toggle();
+      }
 
 })
