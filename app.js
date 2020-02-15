4202186722d581c80a55b282668624edc8f894da
morganBeweb30 <54986200+morganBeweb30@users.noreply.github.com>
2020-02-15 22:31:40 +0100

    typo corrections tech

--- app.js
+++ app.js
@@ -3,63 +3,63 @@ $('.menu-theme').hide();
 $('.solo-quiz').hide();
 
 let quizTech = [{
-    "ask": "Qui est le créateur de Paypal?",
+    "ask": "Qui est le créateur de Paypal ?",
     "choice": ["John Cena", "Elon Musk"],
     "correct": "Elon Musk"
   }, {
-    "ask": "Quel langage permet de rendre des pages web dynamique?",
+    "ask": "Quel langage permet de rendre des pages web dynamiques ?",
     "choice": ["PHP", "JavaScript"],
     "correct": "JavaScript"
   }, {
-    "ask": "Quel vallée des états-unis est réputé pour ses innovations?",
-    "choice": ["Sillicon Valley", "Death Valley"],
-    "correct": "Sillicon Valley"
+    "ask": "Quel vallée des états-unis est réputée pour ses innovations ?",
+    "choice": ["Silicon Valley", "Death Valley"],
+    "correct": "Silicon Valley"
   }, {
-    "ask": "Quel pays a accorder la citoyenneté à un robot?",
+    "ask": "Quel pays a accordé la citoyenneté à un robot ?",
     "choice": ["La Corée du Nord", "L'Arabie Saoudite"],
     "correct": "L'Arabie Saoudite"
   }, {
-    "ask": "Le quel de ces deux site est le plus vieux?",
+    "ask": "Lequel de ces deux sites est le plus vieux ?",
     "choice": ["Twitter", "Facebook"],
     "correct": "Facebook"
   }, {
-    "ask": "A quel année le premier iPhone est apparu?",
+    "ask": "En quelle année le premier iPhone est apparu ?",
     "choice": ["2001", "2007"],
     "correct": "2007"
   }, {
-    "ask": "Quand Internet a été ouvert au public?",
+    "ask": "Quand Internet a été ouvert au public ?",
     "choice": ["1972", "1986"],
     "correct": "1972"
   }, {
-    "ask": "1024mo est égale à combien de Go?",
+    "ask": "1024mo est égal à combien de Go ?",
     "choice": ["1Go", "1To"],
     "correct": "1Go"
   }, {
-    "ask": "Que veux dire IP?",
+    "ask": "Que veut dire IP ?",
     "choice": ["Identification Permise", "Internet Protocol"],
     "correct": "Internet Protocol"
   }, {
-    "ask": "Qu'est-ce que Linux & Windows?",
-    "choice": ["Système d'exploitation", "Des marques"],
-    "correct": "Système d'exploitation"
+    "ask": "Qu'est-ce que Linux & Windows ?",
+    "choice": ["Systèmes d'exploitation", "Des marques"],
+    "correct": "Systèmes d'exploitation"
   }, {
-    "ask": "Qu'est-ce que le Bitcoin?",
+    "ask": "Qu'est-ce que le Bitcoin ?",
     "choice": ["Unité de mesure", "Cryptomonnaie"],
     "correct": "Cryptomonnaie"
   }, {
-    "ask": "Quel est l'utilité d'une dynamo?",
+    "ask": "Quelle est l'utilité d'une dynamo ?",
     "choice": ["Créer de l'énergie", "Créer de l'exploisif"],
     "correct": "Créer de l'énergie"
   }, {
-    "ask": "Qui est l'inventeur de l'imprimante 3D?",
+    "ask": "Qui est l'inventeur de l'imprimante 3D ?",
     "choice": ["Albert Einstein", "Charles Hull"],
     "correct": "Charles Hull"
   }, {
-    "ask": "C'est quoi le service VoIP?",
+    "ask": "Qu'est-ce que le service VoIP ?",
     "choice": ["Voix via Internet", "Voix via Téléphone"],
     "correct": "Voix via Internet"
   }, {
-    "ask": "Qui signifie le Cloud?",
+    "ask": "Que signifie le Cloud ?",
     "choice": ["Un Nuage", "Le stockage distant"],
     "correct": "Le stockage distant"
   }, {
@@ -67,39 +67,39 @@ let quizTech = [{
     "choice": ["Arthur Scherbius", "Alan Turing"],
     "correct": "Alan Turing"
   }, {
-    "ask": "A quel date remonte la conception de l'HDMI?",
+    "ask": "A quelle date remonte la conception de l'HDMI ?",
     "choice": ["2002", "2006"],
     "correct": "2002"
   }, {
-    "ask": "Qui est le fondateur de wikileaks?",
+    "ask": "Qui est le fondateur de wikileaks ?",
     "choice": ["Steve Jobs", "Julian Assange"],
     "correct": "Julian Assange"
   }, {
-    "ask": "Quel est le type de la dernière génération de processeurs?",
+    "ask": "Quel est le type de la dernière génération de processeurs ?",
     "choice": ["LGA", "PGA"],
     "correct": "LGA"
   }, {
-    "ask": "Quel est l'utilité d'un serveur NAS?",
+    "ask": "Quelle est l'utilité d'un serveur NAS ?",
     "choice": ["Stocker des données", "Créer des micro-ondes"],
     "correct": "Stocker des données"
   }, {
-    "ask": "Quel est le plus rapide?",
+    "ask": "Quel est le plus rapide ?",
     "choice": ["SSD", "HDD"],
     "correct": "SSD"
   }, {
-    "ask": "Qu'est-ce qu'un modem?",
+    "ask": "Qu'est-ce qu'un modem ?",
     "choice": ["Service de communication", "Décodeur TV"],
     "correct": "Service de communication"
   }, {
-    "ask": "Quel vitesse vitesse internet est plus rapide?",
+    "ask": "Quel débit internet est plus rapide ?",
     "choice": ["400M/s", "1Gb/s"],
     "correct": "1Gb/s"
   }, {
-    "ask": "Quel extension définie un fichier de compressions?",
+    "ask": "Quelle extension définit un fichier de compression ?",
     "choice": [".pdf", ".zip"],
     "correct": ".zip"
   }, {
-    "ask": "Quel est l'entreprise créatrice de la XBOX?",
+    "ask": "Quelle est l'entreprise créatrice de la XBOX ?",
     "choice": ["Microsoft", "Apple"],
     "correct": "Microsoft"
   }, {
@@ -107,61 +107,61 @@ let quizTech = [{
     "choice": ["Vitesse de Réalité", "Virtual Reality"],
     "correct": "Virtual Reality"
   }, {
-    "ask": "Qui est donc CaptainCrunch?",
+    "ask": "Qui est donc CaptainCrunch ?",
     "choice": ["Un opticien", "Un Hacker"],
     "correct": "Un Hacker"
   }, {
-    "ask": "Cisco et Arbor son quel type de société?",
+    "ask": "Cisco et Arbor sont quel type de société ?",
     "choice": ["Cyber-sécurité", "Hébergement"],
     "correct": "Cyber-sécurité"
   }, {
-    "ask": "Que signifie l'acronyme HP?",
+    "ask": "Que signifie l'acronyme HP ?",
     "choice": ["Hardware-Power", "Hewlett-Packard"],
     "correct": "Hewlett-Packard"
   }, {
-    "ask": "Qu'est-ce que le CES?",
+    "ask": "Qu'est-ce que le CES ?",
     "choice": ["Un évenement technologique", "Une chaine d'information"],
     "correct": "Un évenement technologique"
   }, {
-    "ask": "Que signifie l'acronyme MIT?",
+    "ask": "Que signifie l'acronyme MIT ?",
     "choice": ["Mad Industry of Technology", "Massachusetts Institute of Technology"],
     "correct": "Massachusetts Institute of Technology"
   }, {
-    "ask": "Discord propose quel service?",
+    "ask": "Discord propose quel service ?",
     "choice": ["Service VoIP", "Création de Site Web"],
     "correct": "Service VoIP"
   }, {
-    "ask": "Le quel de ces deux sites a été créer par Kim Dot Com?",
+    "ask": "Lequel de ces deux sites a été créé par Kim Dot Com ?",
     "choice": ["The Pirate Bay", "Megaupload"],
     "correct": "Megaupload"
   }, {
-    "ask": "Quel société à créer Photoshop?",
+    "ask": "Quelle société a créé Photoshop ?",
     "choice": ["Adobe", "Google"],
     "correct": "Adobe"
   }, {
-    "ask": "Quelle unitée de mesure utilise t'on pour mesuré la puissance d'un processeur?",
+    "ask": "Quelle unité de mesure utilise-t-on pour mesurer la puissance d'un processeur ?",
     "choice": ["GHz", "Gb/s"],
     "correct": "GHz"
   }, {
-    "ask": "Que signifie infosec?",
+    "ask": "Que signifie infosec ?",
     "choice": ["Securité Informatique", "Information de sécurité"],
     "correct": "Securité Informatique"
   }, {
-    "ask": "Un Black-Hat est considérer comme?",
+    "ask": "Un Black-Hat est considéré comme ?",
     "choice": ["Mauvais Hacker", "Bon Hacker"],
     "correct": "Mauvais Hacker"
   }, {
-    "ask": "La fsociety est une réelle organisation?",
+    "ask": "La fsociety est une organisation réelle ?",
     "choice": ["Vrai", "Faux"],
     "correct": "Faux"
   }, {
-    "ask": "YouTube est une plate-forme de?",
-    "choice": ["Vidéaste", "Film & Série"],
-    "correct": "Vidéaste"
+    "ask": "YouTube est une plate-forme de ?",
+    "choice": ["Vidéastes", "Film & Série"],
+    "correct": "Vidéastes"
   }, {
-    "ask": "Certain smarthphone on t'il une reconnaissance digital?",
-    "choice": ["Oui, certain", "Impossible"],
-    "correct": "Oui, certain"
+    "ask": "Certains smarthphones ont-ils une reconnaissance digitale ?",
+    "choice": ["Oui, certains", "Impossible"],
+    "correct": "Oui, certains"
   }]
 
 let pseudo;
